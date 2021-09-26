ba4c49c33b6d22e1cd76cc723d902f91b1c05b6b
Fabrice Etiennette <fabrice.etiennette@hotmail.com>
2021-09-27 00:00:00 +0200

    feature/ swiftgen yml added

---
+++ Radiofy/Resources/SwiftGen/Localizable.swift
@@ -0,0 +1,164 @@
+// swiftlint:disable all
+// Generated using SwiftGen — https://github.com/SwiftGen/SwiftGen
+
+import Foundation
+
+// swiftlint:disable superfluous_disable_command file_length implicit_return
+
+// MARK: - Strings
+
+// swiftlint:disable explicit_type_interface function_parameter_count identifier_name line_length
+// swiftlint:disable nesting type_body_length type_name vertical_whitespace_opening_braces
+internal enum L10n {
+  /// About
+  internal static let about = L10n.tr("Localizable", "About")
+  /// Account
+  internal static let account = L10n.tr("Localizable", "Account")
+  /// An email was sent to
+  internal static let anEmailWasSentTo = L10n.tr("Localizable", "An email was sent to")
+  /// This action requires you to enter your password.
+  internal static let askPassword = L10n.tr("Localizable", "askPassword")
+  /// Back
+  internal static let back = L10n.tr("Localizable", "back")
+  /// Cancel
+  internal static let cancel = L10n.tr("Localizable", "Cancel")
+  /// Cancel
+  internal static let cancelButton = L10n.tr("Localizable", "cancelButton")
+  /// Change profile photo
+  internal static let changeProfilePhoto = L10n.tr("Localizable", "Change profile photo")
+  /// Choose from library
+  internal static let chooseFromLibrary = L10n.tr("Localizable", "Choose from library")
+  /// Could not get info, try later.
+  internal static let couldNotGetInfoTryLater = L10n.tr("Localizable", "Could not get info, try later.")
+  /// Could not get radio station, try later
+  internal static let couldNotGetRadioStationTryLater = L10n.tr("Localizable", "Could not get radio station, try later")
+  /// Could not open document, try later.
+  internal static let couldNotOpenDocumentTryLater = L10n.tr("Localizable", "Could not open document, try later.")
+  /// Create account
+  internal static let createAccount = L10n.tr("Localizable", "Create account")
+  /// Delete Account
+  internal static let deleteAccount = L10n.tr("Localizable", "DeleteAccount")
+  /// Duration
+  internal static let duration = L10n.tr("Localizable", "Duration")
+  /// Edit Profile
+  internal static let editProfile = L10n.tr("Localizable", "Edit Profile")
+  /// Email entered is not valid.
+  internal static let emailEnteredIsNotValid = L10n.tr("Localizable", "Email entered is not valid.")
+  /// Episodes
+  internal static let episodes = L10n.tr("Localizable", "Episodes")
+  /// Error
+  internal static let error = L10n.tr("Localizable", "Error")
+  /// Error saving user data
+  internal static let errorSavingUserData = L10n.tr("Localizable", "Error saving user data")
+  /// Error Unavailable
+  internal static let errorUnavailable = L10n.tr("Localizable", "Error Unavailable")
+  /// Good Afternoon
+  internal static let goodAfternoon = L10n.tr("Localizable", "Good Afternoon")
+  /// Good Evening
+  internal static let goodEvening = L10n.tr("Localizable", "Good Evening")
+  /// Good Morning
+  internal static let goodMorning = L10n.tr("Localizable", "Good Morning")
+  /// Good Night
+  internal static let goodNight = L10n.tr("Localizable", "Good Night")
+  /// Go Premium
+  internal static let goPremium = L10n.tr("Localizable", "goPremium")
+  /// Home
+  internal static let home = L10n.tr("Localizable", "Home")
+  /// is temporarily unavailable.
+  internal static let isTemporarilyUnavailable = L10n.tr("Localizable", "is temporarily unavailable.")
+  /// Just
+  internal static let just = L10n.tr("Localizable", "Just")
+  /// Wait 12 hours before start listening again or go premium!
+  internal static let limitMessage = L10n.tr("Localizable", "limitMessage")
+  /// Listening Limit Reached
+  internal static let limitReached = L10n.tr("Localizable", "limitReached")
+  /// LIVE
+  internal static let live = L10n.tr("Localizable", "LIVE")
+  /// Live Radio
+  internal static let liveRadio = L10n.tr("Localizable", "Live Radio")
+  /// Loading...
+  internal static let loading = L10n.tr("Localizable", "Loading...")
+  /// Log in
+  internal static let logIn = L10n.tr("Localizable", "Log in")
+  /// Log out
+  internal static let logOut = L10n.tr("Localizable", "Log out")
+  /// Are you sure you want to log out?
+  internal static let logOutMessage = L10n.tr("Localizable", "LogOutMessage")
+  /// /month.
+  internal static let month = L10n.tr("Localizable", "month.")
+  /// No prior purchases found for your account.
+  internal static let noPriorPurchasesFoundForYourAccount = L10n.tr("Localizable", "No prior purchases found for your account.")
+  /// Photo invalid, try again.
+  internal static let photoInvalidTryAgain = L10n.tr("Localizable", "Photo invalid, try again.")
+  /// Please make sure your password contain a least 8 characters, 1 special character and 1 number.
+  internal static let pleaseMakeSureYourPasswordContainALeast8Characters1SpecialCharacterAnd1Number = L10n.tr("Localizable", "Please make sure your password contain a least 8 characters, 1 special character and 1 number.")
+  /// Please verify your email first
+  internal static let pleaseVerifyYourEmailFirst = L10n.tr("Localizable", "Please verify your email first")
+  /// Privacy Policy
+  internal static let privacyPolicy = L10n.tr("Localizable", "Privacy Policy")
+  /// Ready
+  internal static let ready = L10n.tr("Localizable", "Ready")
+  /// Remove current photo
+  internal static let removeCurrentPhoto = L10n.tr("Localizable", "Remove current photo")
+  /// Reset password
+  internal static let resetPassword = L10n.tr("Localizable", "Reset password")
+  /// Restore Unsuccessful
+  internal static let restoreUnsuccessful = L10n.tr("Localizable", "Restore Unsuccessful")
+  /// Save
+  internal static let save = L10n.tr("Localizable", "Save")
+  /// Search
+  internal static let search = L10n.tr("Localizable", "Search")
+  /// Search your radio
+  internal static let searchYourRadio = L10n.tr("Localizable", "Search your radio")
+  /// Search
+  internal static let searchTab = L10n.tr("Localizable", "SearchTab")
+  /// Settings
+  internal static let settings = L10n.tr("Localizable", "Settings")
+  /// SIGN UP
+  internal static let signUp = L10n.tr("Localizable", "SIGN UP")
+  /// Station unavaible, try later
+  internal static let stationUnavaibleTryLater = L10n.tr("Localizable", "Station unavaible, try later")
+  /// Take photo
+  internal static let takePhoto = L10n.tr("Localizable", "Take photo")
+  /// Terms of Service
+  internal static let termsOfService = L10n.tr("Localizable", "Terms of Service")
+  /// This could be your first name or a nickname.
+  internal static let thisCouldBeYourFirstNameOrANickname = L10n.tr("Localizable", "This could be your first name or a nickname.")
+  /// You can add your favorite radio stations here.
+  internal static let youCanAddYourFavoriteRadioStationsHere = L10n.tr("Localizable", "You can add your favorite radio stations here.")
+  /// Your Library
+  internal static let yourLibrary = L10n.tr("Localizable", "Your Library")
+
+  internal enum AnErrorHadOccurred {
+    /// An error had occurred. Try later.
+    internal static let tryLater = L10n.tr("Localizable", "An error had occurred. Try later.")
+  }
+
+  internal enum NameEnteredIsNotValid {
+    /// Name entered is not valid. 2 characters minimum & 15 Maximum.
+    internal static let _2CharactersMinimum15Maximum = L10n.tr("Localizable", "Name entered is not valid. 2 characters minimum & 15 Maximum.")
+  }
+}
+// swiftlint:enable explicit_type_interface function_parameter_count identifier_name line_length
+// swiftlint:enable nesting type_body_length type_name vertical_whitespace_opening_braces
+
+// MARK: - Implementation Details
+
+extension L10n {
+  private static func tr(_ table: String, _ key: String, _ args: CVarArg...) -> String {
+    let format = BundleToken.bundle.localizedString(forKey: key, value: nil, table: table)
+    return String(format: format, locale: Locale.current, arguments: args)
+  }
+}
+
+// swiftlint:disable convenience_type
+private final class BundleToken {
+  static let bundle: Bundle = {
+    #if SWIFT_PACKAGE
+    return Bundle.module
+    #else
+    return Bundle(for: BundleToken.self)
+    #endif
+  }()
+}
+// swiftlint:enable convenience_type
